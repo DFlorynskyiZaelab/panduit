3961cab6ef4d2a787da43b13144870b02621cbf5
DFlorynskyiZaelab <dmytro.florynskyi@zaelab.com>
2024-07-31 17:07:07 +0300

    github-pages: v1.0

---
+++ docs/chunk-GAIMZ4WN.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-ASYOZRJM.js";import{Z as i,ca as e}from"./chunk-5JLB6RGG.js";var n=(()=>{class t{http;apiUrl="https://api.example.com/data";constructor(r){this.http=r}fetchData(){return this.http.get(this.apiUrl)}static \u0275fac=function(o){return new(o||t)(e(a))};static \u0275prov=i({token:t,factory:t.\u0275fac,providedIn:"root"})}return t})();export{n as a};
